834078f79dd70556de753a727789fcc6ac4c18d3
hansanyue <hansanyue@hotmail.com>
2024-08-18 21:38:33 +0800

    multi client

---
+++ webapp/connection_manager.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from fastapi import WebSocket
+
+
+class ConnectionManager:
+    def __init__(self):
+        self._active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self._active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        if websocket in self._active_connections:
+            self._active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self._active_connections:
+            await connection.send_text(message)
--- webapp/main.py
+++ webapp/main.py
@@ -1,20 +1,28 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 
-from html import HTML
+from html import HTML_MULTI_CLIENT
+
+from connection_manager import ConnectionManager
 
 
 app = FastAPI()
 
+manager = ConnectionManager()
+
 
 @app.get("/")
 async def get():
-    return HTMLResponse(HTML)
+    return HTMLResponse(HTML_MULTI_CLIENT)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
